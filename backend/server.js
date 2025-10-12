02f2fd11d4583d4c00f19fd4148139fdeee3683a
unknown <aditisrivastava7788@gmail.com>
2025-10-13 00:19:47 +0530

    installed mongoose, resolved some connection issues

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 require("dotenv").config();
 const cors=require("cors");
 const path=require("path");
+const connectDB = require('./config/db');
 
 const app=express();
 
@@ -12,6 +13,8 @@ app.use(cors(
         allowedHeaders:["Content-Type", "Authorization"]
     }
 ));
+
+connectDB()
 
 app.use(express.json())
 
