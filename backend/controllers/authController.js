c30ed3853bb26e41bfe8924b4686416429b9b21d
unknown <aditisrivastava7788@gmail.com>
2025-12-09 01:05:23 +0530

    Implemented login and getUserProfile feature. Tested api for login, register and get user profile routes via postman

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,48 +1,76 @@
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const bcrypt=require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
-const generateToken= (userId)=>{
-    return jwt.sign({id : userId}, process.env.JWT_TOKEN, {expiry:"7d"});
-}
+const generateToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_TOKEN, {expiresIn: "7d"
+    });
+};
 
 const registerUser = async (req, res) => {
-    try{
-        const {name, email, password}=req.body;
-        const userExits= User.findOne(email);
-        if(userExits){
-            res.status(400).json({message : "User already exists"});
-        }
-        const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hash(password, salt);
+    try {
+        const { name, email, password } = req.body;
+        const userExists = await User.findOne({ email });
 
-        const user = User.create({
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
             name,
             email,
-            password:hashedPassword
-        })
+            password: hashedPassword,
+        });
 
-        res.status(201).json({
+        return res.status(201).json({
             _id: user._id,
             name: user.name,
             email: user.email,
-            token: generateToken(user._id)
+            token: generateToken(user._id),
         });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Server Error", error: error.message });
     }
-    catch(error){
-        res.status(500).json({message : "Server Error", error : error.message});
-    }
-    
-}
+};
 
 const loginUser = async (req, res) => {
-    console.log("Login successful");
-    
-}
+    try {
+        const {name, email, password}=req.body;
+        let user= await User.findOne({email});
+        if(!user){
+            return res.status(500).json({message: "Invalid email or password"});
+        }
+        const isMatch=await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(500).json({message: "Invalid email or password"});
+        }
+        return res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+
+        })
+        
+    } catch (error) {
+        return res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
 
 const getUserProfile = async (req, res) => {
-    console.log("User profile successful");
-    
-}
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    } catch (error) {
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
 
-module.exports={registerUser, loginUser, getUserProfile};
+module.exports = { registerUser, loginUser, getUserProfile };
