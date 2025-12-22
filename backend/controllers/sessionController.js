579c74d17b96924acabc62cae1ea45146844aeeb
unknown <aditisrivastava7788@gmail.com>
2025-12-23 03:03:27 +0530

    Implementing session creation

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -1,25 +1,42 @@
 const express=require("express");
-const Session=require("../models/Session")
+const Session=require("../models/Session");
+const Session = require("../models/Session");
 
-
+// @desc - create new session and linked question
 const createSession = async (req, res) => {
     try {   
+        const {role, experience, topicsToFocus, description, questions}=req.body;
+        const userId=req.user._id;
+        const Session=new Session.create({
+            user:userId,
+            role, 
+            experience, 
+            topicsToFocus, 
+            description, 
+        });
+        
     } catch (error) {
         return res.status(500).json({success: false, message:"Server Error"});
     }
 }
+
+// @desc - get session by id with populated question 
 const getSessionById = async (req, res) => {
     try {   
     } catch (error) {
         return res.status(500).json({success: false, message:"Server Error"});
     }
 }
+
+// @desc - get all session for logged in user
 const getMySessions = async (req, res) => {
     try {   
     } catch (error) {
         return res.status(500).json({success: false, message:"Server Error"});
     }    
 }
+
+// @desc - delete a session with its questions
 const deleteSession = async (req, res) => {
     try {   
     } catch (error) {
--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const User=require("../models/User");
 const protect = async (req, res, next) => {
     try{
         let token = req.headers.authorization;
-        if(token){
+        if(token && token.startsWith("Bearer")){
             token = token.split(" ")[1];
             const decoded = jwt.verify(token, process.env.JWT_TOKEN);
             req.user = await User.findById(decoded.id).select("-password");
