93680663c1d186df9c2e0367ac67f5fe9cfd8aef
unknown <aditisrivastava7788@gmail.com>
2025-12-27 03:26:24 +0530

    implemented and tested create session endpoint, corrected session schema

--- backend/controllers/sessionController.js
+++ backend/controllers/sessionController.js
@@ -1,22 +1,41 @@
 const express=require("express");
 const Session=require("../models/Session");
-const Session = require("../models/Session");
+const Question=require("../models/Question");
+ 
 
 // @desc - create new session and linked question
 const createSession = async (req, res) => {
     try {   
         const {role, experience, topicsToFocus, description, questions}=req.body;
         const userId=req.user._id;
-        const Session=new Session.create({
+        const session=await Session.create({
             user:userId,
             role, 
             experience, 
             topicsToFocus, 
             description, 
         });
+
+
+
+const questionDocs = await Promise.all(
+  questions.map(async (q, index) => {
+    const question = await Question.create({
+      session: session._id,
+      question: q.question,
+      answer: q.answer,
+    });
+    return question._id;
+  })
+);
+
+
+        session.questions=questionDocs;
+        await session.save();
+        return res.status(201).json({success:true, session});
         
     } catch (error) {
-        return res.status(500).json({success: false, message:"Server Error"});
+        return res.status(500).json({success: false, message: error.message});
     }
 }
 
--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -1,11 +1,11 @@
 const express=require("express");
 const {createSession, getSessionById, getMySessions, deleteSession} = require("../controllers/sessionController");
 
-const router=express.Router;
+const router=express.Router();
 const {protect}=require("../middlewares/authMiddleware");
 
-router.get("/my-sessions", protect, getMySessions);
 router.post("/create", protect, createSession);
+router.get("/my-sessions", protect, getMySessions);
 router.get("/:id", protect, getSessionById);
 router.delete("/:id", protect, deleteSession);
 
