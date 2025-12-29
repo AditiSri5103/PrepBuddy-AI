4e5020f27b1150e7b7bfc24714684c822bb6fdcf
unknown <aditisrivastava7788@gmail.com>
2025-12-29 23:53:04 +0530

    Implemented and tested update note.

--- backend/controllers/quesController.js
+++ backend/controllers/quesController.js
@@ -48,7 +48,15 @@ exports.togglePinQuestion = async (req, res) => {
 // @route   api/questions/:id/note
 exports.updateQuestionNote = async (req, res) => {
     try {
-        
+        const question=await Question.findById(req.params.id);
+        if(!question){
+            return res.status(404).json({success:false, message:"Question not found."});
+        }
+        const {note}=req.body;
+        question.note=note || "";
+        await question.save();
+        res.status(200).json({success:true, question});
+
     }
     catch (error) {
         res.status(500).json({ success: false, message: error.message });
