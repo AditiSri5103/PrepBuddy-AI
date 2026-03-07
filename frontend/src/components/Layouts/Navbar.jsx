d659c9e42cba97217d5e57f5b7c866fcf6d41343
unknown <aditisrivastava7788@gmail.com>
2026-03-08 02:16:53 +0530

    DashBoard and NavBar updated

--- frontend/src/components/Layouts/Navbar.jsx
+++ frontend/src/components/Layouts/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import ProfileInfoCard from "../Cards/ProfileInfoCard";
+import { Link } from "react-router-dom";
 
 const Navbar=()=>{
     return (
-        <div></div>
+        <div className="h-25 bg-white border border-gray-300/50 backdrop-blur-[2px] py-2.5 px-4 md:px-0 sticky top-0 z-30">
+            <div className="container mx-auto flex items-center justify-between gap-5 ">
+                <Link to="/dashboard">
+                <h2 className="text-lg md:text-xl fontmedium text-black font-bold leading-5">Prep Buddy AI</h2>
+                </Link>
+            <ProfileInfoCard/>
+            </div>
+        </div>
     )
 }
 export default Navbar;
--- frontend/src/pages/Home/DashBoard.jsx
+++ frontend/src/pages/Home/DashBoard.jsx
@@ -3,8 +3,10 @@ import DashBoardLayout from '../../components/Layouts/DashBoardLayout';
 import toast from 'react-hot-toast';
 import {LuPlus} from 'react-icons/lu';
 import {CARD_BG} from '../../utils/data';
+import { useNavigate } from 'react-router-dom';
 
 const DashBoard =() =>{
+    const navigate=useNavigate();
     return (
         <DashBoardLayout>DashBoard Page</DashBoardLayout>
     )
