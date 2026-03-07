beaed3fd908904d567485b3327cb7a90a8cc9dbb
unknown <aditisrivastava7788@gmail.com>
2026-03-08 02:36:00 +0530

    Fetching all user sessions from frontend.

--- frontend/src/pages/Home/DashBoard.jsx
+++ frontend/src/pages/Home/DashBoard.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DashBoardLayout from '../../components/Layouts/DashBoardLayout';
 import toast from 'react-hot-toast';
 import {LuPlus} from 'react-icons/lu';
 import {CARD_BG} from '../../utils/data';
 import { useNavigate } from 'react-router-dom';
+import instance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+
 
 const DashBoard =() =>{
     const navigate=useNavigate();
+    const [sessions, setSessions]=useState([]);
+    const[openCreateModal,setOpenCreateModal]=useState(false);
+    const[openDeleteAlert,setOpenDeleteAlert]=useState({
+        open:false,
+        data:null,
+    });
+
+    const fetchAllSession= async()=>{
+        try{
+            const response=await instance.get(API_PATHS.SESSION.GET_ALL);
+            setSessions(response.data);
+        }
+        catch(error){
+            console.log("Error fetching session data ", error);
+        }
+
+    }
+    const deleteSession = async =(sessionData)=>{
+        try{
+            const response=instance.delete(API_PATHS.SESSION.DELETE(sessionData));
+        }
+        catch(error){
+            console.log("Error deleting session: ", error);
+        }
+    }
     return (
         <DashBoardLayout>DashBoard Page</DashBoardLayout>
     )
